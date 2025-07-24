f787334be9d0304054a7a3e558a66d62997b6bf6
jp-knj <kenji.tomita1996@gmail.com>
2025-07-24 10:30:49 +0900

    Remove mistakenly committed build artifacts from packages/renderer/react/

--- packages/renderer/react/src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @minimal-astro/react - React integration for Minimal Astro
- */
-// Main renderer exports
-export { createReactRenderer, createSSRRenderer, createClientRenderer } from './renderer.js';
-// SSR specific exports
-export { createReactSSRRenderer, renderReactComponent } from './ssr.js';
-//# sourceMappingURL=index.js.map
